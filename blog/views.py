4c78e3c456c664263cff97d4de250ae3f0eccbc9
William D. Gizzi <gizzidan@me.com>
2016-12-22 22:34:33 -0500

    add form

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
+
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -11,5 +13,7 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
-
+def post_new(request):
+    form = PostForm()
+    return render(request, 'block/post_edit.html', {'form': form})
 # Create your views here.
